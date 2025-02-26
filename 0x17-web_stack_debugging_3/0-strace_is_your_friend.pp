11df497f8bea6cfad3923740fe7bc904ae1d4532
Alijalal-Dev <159904614+Alijalal-Dev@users.noreply.github.com>
2025-02-26 15:13:47 +0100

    Back to the older version 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,30 +1,6 @@
-# Ensure Apache is installed
+# fixing apache 500 error
 
-package { 'apache2':
-  ensure => installed,
-}
-
-# Ensure the Apache service is running and enabled
-service { 'apache2':
-  ensure    => running,
-  enable    => true,
-  require   => Package['apache2'],
-}
-
-# Fix the 500 error by correcting the .phpp typo in wp-settings.php
-exec { 'fix_phpp_typo':
-  command => 'sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
-  onlyif  => 'grep -q ".phpp" /var/www/html/wp-settings.php',
-  path    => ['/bin', '/usr/bin'],
-  require => Package['apache2'],
-  notify  => Service['apache2'],
-}
-
-# Ensure the wp-settings.php file exists and has correct permissions
-file { '/var/www/html/wp-settings.php':
-  ensure  => file,
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0644',
-  require => Package['apache2'],
+exec { 'Fixing 500 error':
+  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+  provider => shell,
 }
