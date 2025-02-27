2350d8c49e79a415aeb6446330faf32b69e631e9
Alijalal-Dev <aj0546006@gmail.com>
2025-02-27 05:17:14 -0800

    upload another update

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,14 @@
-# fixing apache 500 error
-
-exec { 'Fixing 500 error':
-  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+# Fixing Apache 500 error by correcting the .phpp to .php in wp-settings.php
+exec { 'fix-wordpress':
+  command  => 'sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+  path     => ['/bin', '/usr/bin'],
+  onlyif   => 'grep -q ".phpp" /var/www/html/wp-settings.php',
   provider => shell,
+}
+
+# Restart Apache to apply changes
+service { 'apache2':
+  ensure    => running,
+  enable    => true,
+  subscribe => Exec['fix-wordpress'],
 }
