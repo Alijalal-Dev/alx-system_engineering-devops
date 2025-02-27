3c37d198b5af88fe27519d8f6617d987099ccda9
Alijalal-Dev <159904614+Alijalal-Dev@users.noreply.github.com>
2025-02-27 14:33:01 +0100

    make another full changes 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,14 +1,6 @@
-# Fixing Apache 500 error by correcting the .phpp to .php in wp-settings.php
-exec { 'fix-wordpress':
-  command  => 'sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
-  path     => ['/bin', '/usr/bin'],
-  onlyif   => 'grep -q ".phpp" /var/www/html/wp-settings.php',
-  provider => shell,
-}
+# Fixes bad "phpp" extensions to "php" in "wp-settings.php".
 
-# Restart Apache to apply changes
-service { 'apache2':
-  ensure    => running,
-  enable    => true,
-  subscribe => Exec['fix-wordpress'],
+exec{'fix-wordpress':
+ command => 'sed-i s/phpp/php/g /var/www/html/wp-settings.php',
+ path    => '/usr/local/bin/:/bin/'
 }
