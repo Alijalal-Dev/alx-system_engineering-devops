6426fee52941e1cb5e6780c6f6f179c93cac97d2
Alijalal-Dev <159904614+Alijalal-Dev@users.noreply.github.com>
2025-02-27 14:34:24 +0100

    Back to the oldest version 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
-# Fixes bad "phpp" extensions to "php" in "wp-settings.php".
+# fixing apache 500 error
 
-exec{'fix-wordpress':
- command => 'sed-i s/phpp/php/g /var/www/html/wp-settings.php',
- path    => '/usr/local/bin/:/bin/'
+exec { 'Fixing 500 error':
+  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+  provider => shell,
 }
